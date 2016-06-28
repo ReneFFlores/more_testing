61311cbd040cd0fcea1ae1bf719f2c0f57fc3446
ReneFFlores <ref007_@hotmail.com>
2016-06-27 18:39:07 -0600

    first thn heroku

--- client/main.js
+++ client/main.js
@@ -1,22 +1,84 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+Ingredientes = new Meteor.Collection("Ingredientes");
 
-import './main.html';
+if (Meteor.isClient) {
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+  Template.userManagement.helpers({
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+  });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+  Template.userManagement.events({
+    'click #signup': function() {
+      var user = {
+        username: $('#signup-username').val(),
+        password: $('#signup-password').val(),
+      };
+
+      Accounts.createUser(user, function (error) {
+        if(error) alert(error);
+      });
+    },
+
+    'click #login': function() {
+      var username = $('#login-username').val();
+      var password = $('#login-password').val();
+
+      Meteor.loginWithPassword(username, password, function(error) {
+        if(error) alert(error);
+      });
+    },
+
+    'click #logout': function() {
+      Meteor.logout();
+    }
+  });
+
+
+
+
+  Template.publicarBox.helpers({
+    charCount: function() {
+      return 140 - Session.get('numChars');
+    },
+
+    charClass: function() {
+      if (Session.get('numChars') > 140) {
+        return 'errCharCount';
+      } else {
+        return 'charCount';
+      }
+    },
+
+    disableButton: function() {
+      if (Session.get('numChars') <= 0 ||
+          Session.get('numChars') > 140 ||
+          !Meteor.user()) {
+        return 'disabled';
+      }
+    }
+  });
+
+  Template.publicarBox.events({
+    'input #publicarText': function(){
+      Session.set('numChars', $('#publicarText').val().length);
+    },
+
+    'click button': function() {
+      var publicar = $('#publicarText').val();
+      $('#publicarText').val("");
+      Session.set('numChars', 0);
+      if (Meteor.user()) {
+        Ingredientes.insert({message: publicar, user: Meteor.user().username});
+      }
+    }
+  });
+
+  Template.publicarBox.onRendered(function () {
+    Session.set('numChars', 0);
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
